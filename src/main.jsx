c8572539e78dcc41d4e5125c8ba2b25f213f7227
nelli <nelli.diachkina@gmail.com>
2024-06-12 15:57:53 +0300

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store.js';
+import { store } from './redux/store.js';
 import App from './components/App/App.jsx';
 
 import 'modern-normalize';
@@ -11,9 +10,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
